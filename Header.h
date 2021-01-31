e0a4c9ede4ec7c769af9104afa14e7932bcbab92
Fauxtarantula <68525714+Fauxtarantula@users.noreply.github.com>
2021-01-31 11:18:18 +0800

    Update and rename Read input file code to Header.h

---
+++ Header.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+using namespace std;
+class sauce {
+public:
+	void readFileFun(vector<int> &vect);
+	void shelf(vector<int> &conv);
+	
+};
