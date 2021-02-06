8e865bf32b646cafa61369fe05eff6c230ccd851
Fauxtarantula <68525714+Fauxtarantula@users.noreply.github.com>
2021-02-06 14:59:44 +0800

    Update Header.h

--- Header.h
+++ Header.h
@@ -5,8 +5,17 @@
 #include <list>
 using namespace std;
 class sauce {
-public:
+public:	
+	
+	
 	void readFileFun(vector<int> &vect);
 	void shelf(vector<int> &conv);
+	void Outbox(vector<int>& out);
+	void skip(vector<int>& coc);
+	//void help(vector<vector<int>>)
+
+private:
+	vector<string> rack;
+	vector<int> rackweight;
 	
 };
